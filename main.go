7050fd62b2b7305d8baaefa2769fc781403decdd
Anthony Voutas <anthonyv@kiva.org>
2019-08-30 16:05:18 -0700

    what the

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	addr, set := os.LookupEnv("ADDR")
-	if !set {
-		addr = ":8080"
-	}
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello")
-	})
-
-	log.Fatal(http.ListenAndServe(addr, mux))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
+	http.ListenAndServe(":8080", mux)
 }
