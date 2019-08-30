ca1f5e162040af0f17ed13a3385ff9d8b35bc589
Anthony Voutas <anthonyv@kiva.org>
2019-08-30 16:02:03 -0700

    still huh

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 )
 
 func main() {
-	logger := log.New(os.Stderr, "hello: ", log.Llongfile|log.Lmicroseconds|log.LstdFlags)
-	logger.Println("starting...")
-
 	addr, set := os.LookupEnv("ADDR")
 	if !set {
 		addr = ":8080"
@@ -18,10 +15,8 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logger.Println("hit")
 		fmt.Fprintln(w, "hello")
 	})
 
-	logger.Println("serving on", addr)
-	logger.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
